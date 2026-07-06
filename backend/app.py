35f259988d05b05055b3e1e7d423828c26f3dfe9
Alexander Kwesi <seankwesi24@googlemail.com>
2026-07-06 13:26:42 +0100

    Change host from 'localhost' to '127.0.0.1'

--- backend/app.py
+++ backend/app.py
@@ -1236,4 +1236,4 @@ export default Contact;
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=3001, log_level="info")
+    uvicorn.run(app, host="127.0.0.1", port=3001, log_level="info")
