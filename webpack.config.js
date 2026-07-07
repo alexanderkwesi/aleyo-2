b7873cad218002db4ff09e16838c20aa381a0093
Alexander Kwesi <seankwesi24@googlemail.com>
2026-07-07 17:54:15 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 new webpack.DefinePlugin({
-  "process.env.REACT_APP_API_URL": JSON.stringify(process.env.REACT_APP_API_URL || "https://aleyo-2-six.vercel.app"),
+  "process.env.REACT_APP_API_URL": JSON.stringify(process.env.REACT_APP_API_URL || "https://aleyo-2-1.onrender.com"),
   // Or if you need the whole env object, merge it properly:
   "process.env": JSON.stringify({
     ...process.env,
-    REACT_APP_API_URL: process.env.REACT_APP_API_URL || "https://aleyo-2-six.vercel.app",
+    REACT_APP_API_URL: process.env.REACT_APP_API_URL || "https://aleyo-2-1.onrender.com",
   }),
 }),
