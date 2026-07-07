0fbd1ef53659960043c5ec610c2ad42ae9a40b7c
Alexander Kwesi <seankwesi24@googlemail.com>
2026-07-07 15:41:00 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
     proxy: [
       {
         "/api": {
-          target: "http://127.0.0.1:3001",
+          target: ["http://127.0.0.1:3001", "https://aleyo-2-six.vercel.app", "*"],
           changeOrigin: true,
           secure: false,
         },
