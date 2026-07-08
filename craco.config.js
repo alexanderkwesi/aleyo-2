63398f8f94e08981b0744d7c48f75e969497921e
Alexander Kwesi <seankwesi24@googlemail.com>
2026-07-08 17:27:02 +0100

    Update webpack config to ignore deprecation warnings
    Refactor webpack configuration to ignore deprecation warnings and streamline DefinePlugin usage.

--- craco.config.js
+++ craco.config.js
@@ -4,33 +4,22 @@ const webpack = require('webpack');
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      // Find the DefinePlugin instance
-      const definePlugin = webpackConfig.plugins.find(
-        (plugin) => plugin.constructor.name === 'DefinePlugin'
-      );
-
-      if (definePlugin) {
-        // Get existing definitions
-        const existingDefinitions = definePlugin.definitions || {};
-        
-        // Add your custom definitions WITHOUT overriding process.env entirely
-        definePlugin.definitions = {
-          ...existingDefinitions,
+      // Ignore the deprecation warning
+      webpackConfig.ignoreWarnings = [
+        {
+          message: /fs\.F_OK is deprecated/,
+        },
+      ];
+      
+      // Add environment variables
+      webpackConfig.plugins.push(
+        new webpack.DefinePlugin({
           'process.env.REACT_APP_API_URL': JSON.stringify(
             process.env.REACT_APP_API_URL || 'https://aleyo-2-1.onrender.com'
           ),
-        };
-      } else {
-        // If no DefinePlugin exists, add a new one
-        webpackConfig.plugins.push(
-          new webpack.DefinePlugin({
-            'process.env.REACT_APP_API_URL': JSON.stringify(
-              process.env.REACT_APP_API_URL || 'https://aleyo-2-1.onrender.com'
-            ),
-          })
-        );
-      }
-
+        })
+      );
+      
       return webpackConfig;
     },
   },
