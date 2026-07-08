afe4176a5bde1b51753549992bd8f9ec1a113919
Alexander Kwesi <seankwesi24@googlemail.com>
2026-07-08 15:33:31 +0100

    Add PUBLIC_URL to webpack DefinePlugin
    Added PUBLIC_URL environment variable to DefinePlugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,7 @@
-// webpack.config.js
-const webpack = require('webpack');
-
-module.exports = {
-  // ... other config
-  plugins: [
-    // ✅ CORRECT - Only define specific environment variables
-    new webpack.DefinePlugin({
-      'process.env.REACT_APP_API_URL': JSON.stringify(
-        process.env.REACT_APP_API_URL || 'https://aleyo-2-1.onrender.com'
-      ),
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-    }),
-  ],
-};
+new webpack.DefinePlugin({
+  'process.env.REACT_APP_API_URL': JSON.stringify(
+    process.env.REACT_APP_API_URL || 'https://aleyo-2-1.onrender.com'
+  ),
+  'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+  'process.env.PUBLIC_URL': JSON.stringify(process.env.PUBLIC_URL || ''),
+}),
