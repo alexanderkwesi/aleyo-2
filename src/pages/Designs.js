b1721c003c8a127ccc0f97a576d1f6ee0ab35d2c
Alexander Kwesi <seankwesi24@googlemail.com>
2026-07-09 11:08:29 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- src/pages/Designs.js
+++ src/pages/Designs.js
@@ -58,7 +58,7 @@ const G_END = '#3ED67C';
 const GRAD = `linear-gradient(135deg, ${G_START} 0%, ${G_MID} 50%, ${G_END} 100%)`;
 //const REACT_APP_API_URL = "https://aleyo-2-six.vercel.app";
 // API Base URL
-const API_URL = process.env.REACT_APP_API_URL || 'https://aleyo-2-1.onrender.com';
+const API_URL = process.env.REACT_APP_API_URL || 'https://aleyo-2-1.onrender.com' || "http://127.0.0.1:37976";
 
 // Default templates as fallback when API fails
 const defaultTemplates = [
