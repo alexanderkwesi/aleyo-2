4db411c3c4955b6b92a7ac6cb4a1d6dcfe66f2d5
Alexander Kwesi <seankwesi24@googlemail.com>
2026-07-09 12:29:26 +0100

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -15,7 +15,7 @@ module.exports = {
       webpackConfig.plugins.push(
         new webpack.DefinePlugin({
           'process.env.REACT_APP_API_URL': JSON.stringify(
-            process.env.REACT_APP_API_URL || 'http://35.230.74.10:0'
+            process.env.REACT_APP_API_URL || 'http://0.0.0.0:10000'
           ),
         })
       );
