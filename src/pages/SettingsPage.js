9840b8e20042de00027040199d520f64685a391d
Alexander Kwesi <seankwesi24@googlemail.com>
2026-07-09 11:12:49 +0100

    Update SettingsPage.js

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -89,7 +89,7 @@ const G_MID = '#2DBCB6';
 const G_END = '#3ED67C';
 const GRAD = `linear-gradient(135deg, ${G_START} 0%, ${G_MID} 50%, ${G_END} 100%)`;
 //const REACT_APP_API_URL = "https://aleyo-2-six.vercel.app";
-const API_BASE = process.env.REACT_APP_API_URL || 'https://aleyo-2-1.onrender.com';
+const API_BASE = process.env.REACT_APP_API_URL || 'https://aleyo-2-1.onrender.com' || "http://127.0.0.1:37976" ;
 
 const SettingsPage = () => {
   const theme = useTheme();
