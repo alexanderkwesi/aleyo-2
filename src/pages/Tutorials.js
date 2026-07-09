da6e5e0fb3f83634bfa25b879a422d5c57e908eb
Alexander Kwesi <seankwesi24@googlemail.com>
2026-07-09 11:16:27 +0100

    Update Tutorials.js

--- src/pages/Tutorials.js
+++ src/pages/Tutorials.js
@@ -81,7 +81,7 @@ const G_MID = '#2DBCB6';
 const G_END = '#3ED67C';
 const GRAD = `linear-gradient(135deg, ${G_START} 0%, ${G_MID} 50%, ${G_END} 100%)`;
 //const REACT_APP_API_URL = "https://aleyo-2-six.vercel.app";
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://aleyo-2-1.onrender.com' || "http://127.0.0.1:37976" ;
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://aleyo-2-1.onrender.com' || "http://127.0.0.1:*" ;
 
 // Category icons mapping
 const CATEGORY_ICONS = {
